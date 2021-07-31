fd8ad67bf82dbae414202c401e81d1bf47ddf469
llhuang10 <llhuang10@iflytek.com>
2021-07-31 19:49:09 +0800

    feat: 添加路由导航守卫来控制访问权限[必须登录有token], 并新增了vue.config.js配置文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -13,7 +14,27 @@ const router = new VueRouter({
     {
       path: '/login',
       component: Login
+    }, {
+      path: '/home',
+      component: Home
     }]
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to   将要访问的路径
+  // from 代表从按个路径跳转而来
+  // next 是一个函数, 表示放行
+  //      next() 放行  next('/login') 强行跳转
+  if (to.path === '/login') {
+    return next()
+  }
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) {
+    return next('/login')
+  }
+  next()
 })
 
 export default router
