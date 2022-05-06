0f688ad38a5aa838d56f6a3d154600020fe3390b
Geda-999 <1322001816@qq.com>
2022-05-06 21:18:40 +0800

    54添加用户-宣染添加用户的表单

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -69,11 +69,24 @@
 
         <!-- 添加用户的对话框 -->
         <el-dialog
-            title="提示"
+            title="添加用户"
             :visible.sync="addDialgVisible"
             width="50%">
             <!-- 内容主体区 -->
-            <span>这是一段信息</span>
+            <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+                <el-form-item label="用户名" prop="username">
+                    <el-input v-model="addForm.username"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                    <el-input v-model="addForm.password"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱" prop="email">
+                    <el-input v-model="addForm.email"></el-input>
+                </el-form-item>
+                <el-form-item label="手机" prop="mobile">
+                    <el-input v-model="addForm.mobile"></el-input>
+                </el-form-item>
+            </el-form>
             <!-- 底部按钮区 -->
             <span slot="footer" class="dialog-footer">
                 <el-button @click="addDialgVisible = false">取 消</el-button>
@@ -99,7 +112,31 @@ export default {
       userlist: [], // 获取所有用户列表
       total: 0, // 总数据调数
       // 控制添加用户对话框的显示与隐藏
-      addDialgVisible: false
+      addDialgVisible: false,
+      // 添加用户的表单数据
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      // 添加表单的验证规则对象
+      addFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '用户名的长度在3~10个字符之间', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '用户名的长度在6~15个字符之间', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' }
+        ]
+      }
     }
   },
   // Vue的生命周期
