20326271d32e5f3455e2db2e86511aca8b1ecb6d
Geda-999 <1322001816@qq.com>
2022-05-06 13:50:47 +0800

    46表格添加索引列

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -24,6 +24,7 @@
             <!-- 用户列表区域 -->
             <!-- :data是数据源  border：边框线 stripe：各行变色效果-->
             <el-table :data="userlist" border stripe class="mt-4 text-xs">
+                <el-table-column type="index"></el-table-column>
                 <el-table-column label="姓名" prop="username"></el-table-column>
                 <el-table-column label="邮箱" prop="email"></el-table-column>
                 <el-table-column label="电话" prop="mobile"></el-table-column>
