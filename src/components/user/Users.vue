9471733699c8e25ed8b839924e576f8629998f9f
Geda-999 <1322001816@qq.com>
2022-05-07 22:02:30 +0800

    57添加用户-调用API接口完成添加用户的操作

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -91,7 +91,7 @@
             <!-- 底部按钮区 -->
             <span slot="footer" class="dialog-footer">
                 <el-button @click="addDialgVisible = false">取 消</el-button>
-                <el-button type="primary" @click="addDialgVisible = false">确 定</el-button>
+                <el-button type="primary" @click="addUser">确 定</el-button>
             </span>
         </el-dialog>
    </div>
@@ -213,8 +213,28 @@ export default {
       }
       this.$message.success('更新用户状态成功！')
     },
+    // 监听添加用户对话框的关闭事件
     addDialogClosed() {
       this.$refs.addFormRef.resetFields()
+    },
+    // 点击按钮，添加新用户的表单预校验
+    addUser() {
+      this.$refs.addFormRef.validate(async valid => {
+        // console.log(valid)
+        if (!valid) return
+        // 可以发起添加用户的网络请求
+        const { data: res } = await this.$http.post('users', this.addForm)
+
+        if (res.meta.status !== 201) {
+          this.$message.error('添加用户失败！')
+        }
+
+        this.$message.success('添加用户成功')
+        // 隐藏添加用户的对话框
+        this.addDialgVisible = false
+        // 重新获取用户列表数据
+        this.getUserList()
+      })
     }
 
   }
