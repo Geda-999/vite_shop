4d503c6b8afa31ad53c30532145a430539b614ad
Geda-999 <1322001816@qq.com>
2022-05-08 14:39:17 +0800

    63修改用户-提交表单完成用户信息的修改

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -293,8 +293,30 @@ export default {
     },
     // 修改用户信息并提交表单 【表单预设校验】
     editUserInfo() {
-      this.$refs.editFromRef.validate(valid => {
-        console.log(valid)
+      this.$refs.editFromRef.validate(async valid => {
+        // console.log(valid)
+        if (!valid) return
+        // 发起修改用户信息的数据请求
+        // 发送网络请求    用户      id     信息
+        // eslint-disable-next-line no-sequences
+        const { data: res } = await this.$http.put('users/' + this.editFrom.id, {
+          email: this.editFrom.email,
+          mobile: this.editFrom.mobile
+        })
+
+        // 判断用户信息
+        if (res.meta.status !== 200) {
+          return this.$message.error('更新用户信息失败！')
+        }
+
+        // 必备三步走
+
+        // 关闭对话框
+        this.editDialogVisible = false
+        // 刷新数据列表
+        this.getUserList()
+        // 提示修改成功
+        this.$message.success('更新用户信息成功！')
       })
     }
 
