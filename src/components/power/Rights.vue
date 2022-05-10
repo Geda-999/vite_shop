4ca80518e926bb3cb1df7df0ed27afbcd6dc7d25
Geda-999 <1322001816@qq.com>
2022-05-10 21:28:35 +0800

    69权限列表绘制面包屑导航和卡片视图

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        <h3>权限列表组件页面</h3>
+        <!-- 面包屑导航栏区域 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right" class="mb-4 text-xs">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+            <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <!-- 卡片视图区域 -->
+        <el-card>
+            卡片视图区域
+        </el-card>
     </div>
 </template>
 
