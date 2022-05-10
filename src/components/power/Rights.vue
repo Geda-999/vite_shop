8e35ac1a65a2c252c7f2436b02b9386d8f0a2e22
Geda-999 <1322001816@qq.com>
2022-05-10 22:54:31 +0800

    71权限列表-宣染权限列表U结构

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -9,7 +9,19 @@
 
         <!-- 卡片视图区域 -->
         <el-card>
-            卡片视图区域
+            <el-table :data="rightsList" border>
+                <el-table-column type="index"></el-table-column>
+                <el-table-column label="权限名称" prop="authName"></el-table-column>
+                <el-table-column label="路径" prop="path"></el-table-column>
+                <el-table-column label="权限等级" prop="level">
+                    <!-- 插槽 Tag 标签 -->
+                    <template slot-scope="scope">
+                        <el-tag v-if="scope.row.level==='0'">一级</el-tag>
+                        <el-tag v-else-if="scope.row.level==='1'" type="success">二级</el-tag>
+                        <el-tag v-else type="warning">三级</el-tag>
+                    </template>
+                </el-table-column>
+            </el-table>
         </el-card>
     </div>
 </template>
