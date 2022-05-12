221fa31d96a42563d0a076f2160fdc8b7e68d566
Geda-999 <1322001816@qq.com>
2022-05-12 23:48:57 +0800

    98商品分类-通过路由加载商品分类组件

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        商品分类组件
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
