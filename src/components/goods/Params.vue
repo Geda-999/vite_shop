26aba25439d6d887258d1bcbe3f33bc7754f9295
Geda-999 <1322001816@qq.com>
2022-05-14 09:01:44 +0800

    116分类参数-调用API获取商品分类列表数据

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -1,52 +1,68 @@
 <template>
-    <div>
-        <!-- 面包屑导航栏区域 -->
-        <el-breadcrumb separator-class="el-icon-arrow-right" class="mb-4 text-xs">
-            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-            <el-breadcrumb-item>商品管理</el-breadcrumb-item>
-            <el-breadcrumb-item>参数列表</el-breadcrumb-item>
-        </el-breadcrumb>
+  <div>
+    <!-- 面包屑导航栏区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right" class="mb-4 text-xs">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
 
-        <!-- 卡片视图区域 -->
-        <el-card>
-            <!-- 警告区域  Alert 警告 -->
-            <el-alert
-                title="注意：只允许为第三级分类设置相关参数！"
-                type="warning"
-                show-icon
-                :closable="false">
-            </el-alert>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 警告区域  Alert 警告 -->
+      <el-alert
+        title="注意：只允许为第三级分类设置相关参数！"
+        type="warning"
+        show-icon
+        :closable="false"
+      >
+      </el-alert>
 
-            <!-- 选择商品分类区域 -->
-            <!-- 一行 -->
-            <el-row class="my-4">
-                <!-- 一列 -->
-                <el-col>
-                    <span>选择商品分类：</span>
+      <!-- 选择商品分类区域 -->
+      <!-- 一行 -->
+      <el-row class="my-4">
+        <!-- 一列 -->
+        <el-col>
+          <span>选择商品分类：</span>
 
-                    <!-- 选择商品分类的级联选择框 -->
-                </el-col>
-            </el-row>
-        </el-card>
-    </div>
+          <!-- 选择商品分类的级联选择框 -->
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-
+      // 获取所有 商品分类列表
+      cateList: []
     }
   },
   created() {
-
+    // 咋们来调用一个方法
+    this.getCateList()
   },
   methods: {
+    async getCateList() {
+      // 通过this.$http 发起get请求
+      const { data: res } = await this.$http.get('categories')
 
+      // 判断环节
+      if (res.meta.status !== 200) {
+        //   如果失败就提示以下内容
+        return this.$message.error('获取商品分类失败！')
+      }
+
+      // 成功之后
+      // 注意： 获取所有商品 一定要先赋值到data中共页面使用
+      this.cateList = res.data
+
+      console.log(this.cateList)
+    }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
