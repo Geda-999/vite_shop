a6ce945c6fb718479d1fcf49bb19112cd8430b51
Geda-999 <1322001816@qq.com>
2022-05-16 15:28:25 +0800

    153商品添加-实现步骤条和ab栏的数据联动效果

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -14,7 +14,9 @@
 
       <!-- 步骤条区域 -->
       <!-- active这个不要写死他 要判定到data中才灵活 -->
-      <el-steps :space="200" :active="activeIndex" finish-status="success" align-center>
+      <!-- 字符串转换数字 -->
+      <!-- active 如果你要将一个长得 像数字的字符串 转换成数字的话 以最简单的方法就是给他【-0】就行了 -->
+      <el-steps :space="200" :active="activeIndex - 0" finish-status="success" align-center>
         <el-step title="基本信息"></el-step>
         <el-step title="商品参数"></el-step>
         <el-step title="商品属性"></el-step>
@@ -24,11 +26,14 @@
       </el-steps>
 
       <!-- tab栏区域 -->
-      <el-tabs :tab-position="'left'" style="height: 200px">
-        <el-tab-pane label="基本信息">基本信息</el-tab-pane>
-        <el-tab-pane label="商品参数">商品参数</el-tab-pane>
-        <el-tab-pane label="商品图片">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+      <!-- v-model判定到 步骤条 :active身上 从而来获取联动信息  -->
+      <!-- name="0"就是代码 步骤条 里的第一个 -->
+      <el-tabs v-model="activeIndex" :tab-position="'left'" style="height: 200px">
+        <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
       </el-tabs>
     </el-card>
   </div>
@@ -40,7 +45,8 @@ export default {
   // 这是私有数据
   data() {
     return {
-      activeIndex: 0,
+      // 商品添加-实现步骤条和ab栏的数据联动效果
+      activeIndex: '0',
     }
   },
   // 这是生命周期函数
